9c489d0d800ed44b359a0b487004e68f6c65ae49
AmirMohammadHakimiyan <amirmohammadhakimiyan6@gmail.com>
2023-03-16 04:49:12 -0700

    pyhon gui and threading

--- ASsignment__24/main_2.py
+++ ASsignment__24/main_2.py
@@ -1,10 +1,24 @@
+import random
 import threading
 from moviepy import editor 
 from functools import partial
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from ui_file import Ui_MainWindow
+class My_thread(QThread):
+    qwee=Signal()
+    def __init__(self,lt):
+        super().__init__()
+        self.path=lt
+
+    def run(self):
+        video=editor.VideoFileClip(self.path)
+        self.name=str(random.randint(0,10000000000))+"_clip.mp3"
+        video.audio.write_audiofile(self.name)
+        self.qwee.emit()
+        
 class MainWindow(QMainWindow):
+    read=Signal(list)
     def __init__(self):
         super().__init__()
         self.ui=Ui_MainWindow()
@@ -16,11 +30,13 @@ class MainWindow(QMainWindow):
         self.box.setText("plesse, complte all of them!😎")
         self.box_2=QMessageBox()
         self.box_2.setWindowTitle("***ABOUT***")
-        self.box_2.setText("1_it can to convert 5 vidio files into audio files✔\n2_it can't convert very fast❌")
+        self.box_2.setText("1_it can to convert 5 vidio files into audio files✔\n2_it can convert very fast✔")
         self.box_2.show()
         for i in range(5):
             self.line[i].textChanged.connect(partial(self.edit_box,self.line[i]))
         self.ui.pushButton.clicked.connect(partial(self.run))
+        self.number_1=0
+        self.number_2=0
     def edit_box(self,m,n):
         if len(m.text())<3:
             file_path=QFileDialog.getOpenFileName(self,"choce your file!")
@@ -34,12 +50,42 @@ class MainWindow(QMainWindow):
         for i in range(5):
             if self.line[i].text()=="":
                 self.box.show()
+                self.number_1+=1
             else:
-                vidio= editor.VideoFileClip(self.all_file[i])
-                clip=str(i)+"_clip.mp3"
-                vidio.audio.write_audiofile(clip)
+                self.number_2+=1
+                if self.number_2==5 and self.number_1==0:
+                    self.read.emit(self.all_file)
+        self.number_1=0
+        self.number_2=0
+                    
+def maker(lt):
+    global th
+    th=My_thread(lt[0])
+    th_2=My_thread(lt[1])
+    th_3=My_thread(lt[2])
+    th_4=My_thread(lt[3])
+    th_5=My_thread(lt[4])
+    th_3.start()
+    th_4.start()
+    th_5.start()
+    th_2.start()
+    th.start()
+
+    th_2.qwee.connect(th_2.quit)
+    th_3.qwee.connect(th_3.quit)
+    th_4.qwee.connect(th_4.quit)
+    th_5.qwee.connect(th_5.quit)
+    th.qwee.connect(th.quit)
+    th_2.wait()
+    th_3.wait()
+    th_4.wait()
+    th_5.wait()
+    th.wait()
+
 if __name__=="__main__":
     app=QApplication()
     window=MainWindow()
+    window.read.connect(maker)
+
     window.show()
     app.exec()
--- ASsignment__24/readme.md
+++ ASsignment__24/readme.md
@@ -1,5 +1,9 @@
 # ***PYTHON***
-## file edite (old version)
-it's can to edit five file
+## file edite 1
+### it's into a main_1.py (old version)
+it can to edit five file.
+## file edit 2
+### it's into a main_2.py (new version)
+it can to edit five file and it can to convert all the files with toghter.
 ![](picture.jpg)
 
